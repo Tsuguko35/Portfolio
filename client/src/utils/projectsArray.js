ff4dc2740f52e8fe785c9c808d28f31d3db81270
Tsuguko35 <carpio.johnjazpher.dc.3188@gmail.com>
2024-07-06 20:23:29 +0800

    new links

--- client/src/utils/projectsArray.js
+++ client/src/utils/projectsArray.js
@@ -9,13 +9,15 @@ const projects_list = [
     image:
       "https://res.cloudinary.com/dkwgg59ur/image/upload/v1719018310/Portfolio_Files/Projects/oogetlao87n038uhq7lv.webp",
     title: "Arc Corporation",
-    link: "https://goarccorp.com/",
+    link: "https://arc-corp.vercel.app/",
+    // link: "https://goarccorp.com/",
   },
   {
     image:
       "https://res.cloudinary.com/dkwgg59ur/image/upload/v1719018310/Portfolio_Files/Projects/e80d0lodbcesfahxwx1o.webp",
     title: "Eden Home",
-    link: "https://goedenhome.com/",
+    link: "https://eden-development.vercel.app/",
+    // link: "https://goedenhome.com/",
   },
   {
     image:
